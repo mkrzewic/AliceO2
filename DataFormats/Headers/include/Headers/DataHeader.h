982581419c65c17e70df83c6dac1f71dcadf5a0e
mkrzewic <mikolaj.krzewicki@cern.ch>
2018-05-11 16:43:46 +0200

    some fix

--- DataFormats/Headers/include/Headers/DataHeader.h
+++ DataFormats/Headers/include/Headers/DataHeader.h
@@ -720,7 +720,7 @@ constexpr o2::header::DataDescription gDataDescriptionTracks{ "TRACKS" };
 constexpr o2::header::DataDescription gDataDescriptionConfig{ "CONFIGURATION" };
 constexpr o2::header::DataDescription gDataDescriptionInfo{ "INFORMATION" };
 constexpr o2::header::DataDescription gDataDescriptionROOTStreamers{ "ROOT STREAMERS" };
-constexpr o2::header::DataDescription gDataDescriptionVertexCoordinates{ "VERTEX COORDINATES" };
+constexpr o2::header::DataDescription gDataDescriptionVertexCoordinates{ "VTX" };
 /// @} // end of doxygen group
 
 //__________________________________________________________________________________________________
